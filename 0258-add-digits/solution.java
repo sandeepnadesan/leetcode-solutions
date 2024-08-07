e2deb8f8f7c91ceff66b6aa280d13e64bb51d651
Sandeep N <sandeepnadesankns@gmail.com>
2024-08-07 15:40:08 +0000

    [LeetCode Sync] Runtime - 1 ms (47.53%), Memory - 41.4 MB (11.17%)

---
+++ 0258-add-digits/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int addDigits(int num) {
+   
+        num = Math.abs(num);
+        
+     
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        
+        return num;
+    }
+}
+
