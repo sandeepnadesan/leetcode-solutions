56b75d09b1037a05ac905c9bba636a204b105429
Sandeep N <sandeepnadesankns@gmail.com>
2024-09-13 16:11:56 +0000

    [LeetCode Sync] Runtime - 47 ms (15.03%), Memory - 44.5 MB (93.34%)

--- 0001-two-sum/solution.java
+++ 0001-two-sum/solution.java
@@ -1,48 +1,18 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-
     public int[] twoSum(int[] nums, int target) {
-        // Create a HashMap to store the complement of each element
-        Map<Integer, Integer> complementMap = new HashMap<>();
-
-        // Iterate through the array
-        for (int i = 0; i < nums.length; i++) {
-            // Calculate the complement
-            int complement = target - nums[i];
-
-            // Check if the complement is in the map
-            if (complementMap.containsKey(complement)) {
-                // Return the indices of the two numbers
-                return new int[]{complementMap.get(complement), i};
+        int n= nums.length;
+		int arr1[] = new int[2];
+        
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    arr1[0]=i;
+                    arr1[1]=j;
+                    return arr1;
+                    
+                }
             }
-
-            // If not found, add the current number and its index to the map
-            complementMap.put(nums[i], i);
         }
-
-        // If no solution is found (which should not happen given the problem constraints)
-        return null;
-    }
-
-    public static void main(String[] args) {
-        // Example usage:
-        int[] nums1 = {2, 7, 11, 15};
-        int target1 = 9;
-        int[] result1 = new Solution().twoSum(nums1, target1);
-        System.out.println(Arrays.toString(result1));  // Output: [0, 1]
-
-        int[] nums2 = {3, 2, 4};
-        int target2 = 6;
-        int[] result2 = new Solution().twoSum(nums2, target2);
-        System.out.println(Arrays.toString(result2));  // Output: [1, 2]
-
-        int[] nums3 = {3, 3};
-        int target3 = 6;
-        int[] result3 = new Solution().twoSum(nums3, target3);
-        System.out.println(Arrays.toString(result3));  // Output: [0, 1]
+    return null;
     }
 }
-
