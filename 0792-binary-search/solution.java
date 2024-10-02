c99a600f5067ba3c19ec19cbb2c3a745a9eb5704
Sandeep N <sandeepnadesankns@gmail.com>
2024-10-02 05:45:07 +0000

    [LeetCode Sync] Runtime - 1 ms (11.07%), Memory - 45.5 MB (78.10%)

---
+++ 0792-binary-search/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int a=nums.length;
+        
+        for(int i=0;i<a;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+         return -1;
+
+
+    }
+   
+    
+}
+    
