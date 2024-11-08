c18800e54dd93a6bc72dda7163de320cbb32d8d2
Sandeep N <sandeepnadesankns@gmail.com>
2024-11-08 04:31:46 +0000

    [LeetCode Sync] Runtime - 6 ms (30.88%), Memory - 46.5 MB (21.10%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            ans = ans ^ nums[i];
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i+=2){
+            if(nums[i]!=nums[i+1])return nums[i];
         }
-        return ans;
+        return nums[nums.length-1];
     }
 }
