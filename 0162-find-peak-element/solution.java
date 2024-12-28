c60253fdab278fd1ccd89144f03d2a141ab61001
Sandeep N <sandeepnadesankns@gmail.com>
2024-12-28 12:54:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (62.07%)

--- 0162-find-peak-element/solution.java
+++ 0162-find-peak-element/solution.java
@@ -1,21 +1,20 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left < right) {
-            int mid =(left + right) / 2;
-            
-            if (nums[mid] > nums[mid + 1]) {
-                // Peak is in the left half, including mid
-                right = mid;
-            } else {
-                // Peak is in the right half, excluding mid
-                left = mid + 1;
+        int n=nums.length;
+        int r=n-1;
+        int l=0;
+        if(n==1){
+            return 0;
+        }
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid]>nums[mid+1]){
+                r=mid;
+            }
+            else{
+                l=mid+1;
             }
         }
-        
-        return left; // or right, since left == right at the end
+        return l;
     }
 }
-
