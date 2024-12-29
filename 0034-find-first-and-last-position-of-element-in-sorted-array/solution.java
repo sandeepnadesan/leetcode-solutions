43b00f479f65c030df516a1c9fb81eaf6c055791
Sandeep N <sandeepnadesankns@gmail.com>
2024-12-29 08:53:38 +0000

    [LeetCode Sync] Runtime - 3 ms (2.78%), Memory - 46.2 MB (22.48%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,36 +1,62 @@
-class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
-    }
-
-    private int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
-        int left = 0;
-        int right = nums.length - 1;
-        int idx = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+class Solution {    
+    public int start(int []nums,int t){
+        int n=nums.length;
+        int l=0;
+        int r=n-1;
+        int val=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==t){
+                val=mid;
+                r=mid-1;
+                System.out.println(r);
+                
             }
+            else if(nums[mid]>t){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+            
         }
-
-        return idx;
+        return val;
+    }
+    public int end(int []nums,int t){
+        int n=nums.length;
+        int l=0;
+        int r=n-1;
+        int val=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==t){
+                val=mid;
+                l=mid+1;
+                System.out.println(val);
+                
+            }
+            else if(nums[mid]>t){
+                r=mid-1;
+                
+            }
+            else{
+                l=mid+1;
+            }
+            
+        }
+        return val;
     }
 
+
+
+
+    public int[] searchRange(int[] nums, int t) {
+        int st=start(nums,t);
+        int end=end(nums,t);
+        int ans[]=new int [2];
+        ans[0]=st;
+        ans[1]=end;
+        return ans;
+    }
 }
+
