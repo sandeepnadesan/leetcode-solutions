0a47944e6e0684ef7a04673da497c88e45882d2a
Sandeep N <sandeepnadesankns@gmail.com>
2024-12-30 12:51:40 +0000

    [LeetCode Sync] Runtime - 5 ms (28.56%), Memory - 46.5 MB (8.53%)

--- 0004-median-of-two-sorted-arrays/solution.java
+++ 0004-median-of-two-sorted-arrays/solution.java
@@ -1,30 +1,24 @@
 class Solution {
-    private int p1 = 0, p2 = 0;
-
-    // Get the smaller value between nums1[p1] and nums2[p2] and move the pointer forwards.
-
-    private int getMin(int[] nums1, int[] nums2) {
-        if (p1 < nums1.length && p2 < nums2.length) {
-            return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
-        } else if (p1 < nums1.length) {
-            return nums1[p1++];
-        } else if (p2 < nums2.length) {
-            return nums2[p2++];
-        } 
-        return -1;
-    }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length;
-        if ((m + n) % 2 == 0) {
-            for (int i = 0; i < (m + n) / 2 - 1; ++i) {
-                int tmp = getMin(nums1, nums2);
-            }
-            return (double)(getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
-        } else {
-            for (int i = 0; i < (m + n) / 2; ++i) {
-                int tmp = getMin(nums1, nums2);
-            }
-            return getMin(nums1, nums2);
-        }     
+        int []temp=new int[nums1.length+nums2.length];
+        int k=0;
+        for(int i=0;i<nums1.length;i++)
+        {
+            temp[k++]=nums1[i];
+        }
+        for(int i=0;i<nums2.length;i++)
+        {
+            temp[k++]=nums2[i];
+        }
+        Arrays.sort(temp);
+        
+        if(temp.length%2==0)
+        {
+            return ((double)temp[temp.length/2]+(double)temp[temp.length/2-1])/2.0;
+        }
+        else
+        {
+            return (double)(temp[temp.length/2]);
+        }
     }
 }
