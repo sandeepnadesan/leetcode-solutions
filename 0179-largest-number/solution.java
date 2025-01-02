ab228f63b817aa5d0ae59e4be84835fc4ba5142e
Sandeep N <sandeepnadesankns@gmail.com>
2025-01-02 13:15:24 +0000

    [LeetCode Sync] Runtime - 6 ms (52.77%), Memory - 43.5 MB (39.50%)

--- 0179-largest-number/solution.java
+++ 0179-largest-number/solution.java
@@ -1,18 +1,19 @@
-	class Solution {
-		public String largestNumber (int[] nums) {
-			int n = nums.length;
-			String[] s = new String[n];
-			for (int i = 0; i < n; i++)
-				s[i] = nums[i] + "";
-			Arrays.sort(s, (s1, s2) -> (s1 + s2).compareTo(s2 + s1));
-			StringBuilder res = new StringBuilder();
-			String checkZero = "";
-			for (int i = n - 1; i >= 0; i--) {
-				res.append(s[i]);
-				checkZero = checkZero + '0';
-			}
-			if (res.toString().equals(checkZero))
-				return "0";
-			return res.toString();
-		}
-	}
+class Solution {
+    public String largestNumber(int[] nums) {
+        int n=nums.length;
+        String [] s= new String[n];
+        for(int i=0;i<n;i++){
+            s[i]=nums[i]+"";
+        }
+        Arrays.sort(s,(s1,s2)->(s1+s2).compareTo(s2+s1));
+        StringBuilder sb =  new StringBuilder();
+        String zc="";
+        for(int i=n-1;i>=0;i--){
+            sb.append(s[i]);
+            zc+='0';
+        }
+        if(sb.toString().equals(zc))return "0";
+        else
+            return sb.toString();
+    }
+}
