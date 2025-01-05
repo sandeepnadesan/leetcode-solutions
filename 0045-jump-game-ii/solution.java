07ce31eb5531fc18c1bd8cbc59b0973ec2951f7a
Sandeep N <sandeepnadesankns@gmail.com>
2025-01-05 06:15:19 +0000

    [LeetCode Sync] Runtime - 1 ms (99.25%), Memory - 45 MB (67.00%)

---
+++ 0045-jump-game-ii/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int jumps = 0;
+        int l = 0; 
+        int r = 0;  
+        for (int i = 0; i < n - 1; i++) { 
+            l = Math.max(l, i + nums[i]);
+            if (i == r) {
+                r = l;   
+                jumps++; 
+            }
+        }
+        return jumps; 
+    }
+}
