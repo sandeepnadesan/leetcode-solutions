52accb7a82085a8141d81ca424a016e133bdc9fd
Sandeep N <sandeepnadesankns@gmail.com>
2025-01-07 13:15:34 +0000

    [LeetCode Sync] Runtime - 3 ms (98.72%), Memory - 43.9 MB (99.11%)

--- 0012-integer-to-roman/solution.java
+++ 0012-integer-to-roman/solution.java
@@ -1,20 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
-
-        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String ths[]={"","M","MM","MMM"};
-        
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
-};
-
-    public static void main(String[] args){
-        Solution solution = new Solution();
-        System.out.println(solution.intToRoman(3));
-        System.out.println(solution.intToRoman(58));
-        System.out.println(solution.intToRoman(1994));
-  
-
+        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length; ++i) {
+            if (num == 0)
+                break;
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return sb.toString();        
     }
 }
