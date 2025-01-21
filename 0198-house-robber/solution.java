fa2273ffd442b249f7b82b18a976f09d018af6ed
Sandeep N <sandeepnadesankns@gmail.com>
2025-01-21 10:43:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (24.07%)

--- 0198-house-robber/solution.java
+++ 0198-house-robber/solution.java
@@ -1,18 +1,16 @@
 class Solution {
-    public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    public int sol(int nums[],int i,int dp[]){
+        int n=nums.length;
+        if(i>=n){
+            return 0;
         }
-
-        return dp[n - 1];
+        if(dp[i]!=-1)return dp[i];
+        return dp[i]=Math.max((nums[i]+sol(nums,i+2,dp)),sol(nums,i+1,dp));
+    }
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return sol(nums,0,dp);
     }
 }
-
