36ed469b2d684459a9b37693cdd2bc486da7296a
Sandeep N <sandeepnadesankns@gmail.com>
2025-03-02 12:57:14 +0000

    [LeetCode Sync] Runtime - 1 ms (82.81%), Memory - 44.3 MB (29.52%)

---
+++ 2551-apply-operations-to-an-array/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n=nums.length;
+        int res[]=new int[n];
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        int ind=0;
+        for(int num:nums){
+            if(num!=0){
+                res[ind++]=num;
+            }
+        }
+        return res;
+    }
+}
