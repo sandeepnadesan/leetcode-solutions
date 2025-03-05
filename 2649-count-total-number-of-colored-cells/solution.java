7d1ba2457203fe9a22d1b6c45fc88dbe3160bd85
Sandeep N <sandeepnadesankns@gmail.com>
2025-03-05 09:54:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.6 MB (36.27%)

---
+++ 2649-count-total-number-of-colored-cells/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long coloredCells(int n) {
+        // long t = n*n ;
+        // long g = (n-1)*(n-1);
+
+
+        return (long) n * n + (long) (n - 1) * (n - 1);
+    }
+}
