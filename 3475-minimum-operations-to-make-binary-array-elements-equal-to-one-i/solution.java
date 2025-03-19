9d3d54287705b421b4eeffb453f2659492bc2c0e
Sandeep N <sandeepnadesankns@gmail.com>
2025-03-19 16:15:53 +0000

    [LeetCode Sync] Runtime - 6 ms (99.81%), Memory - 57.2 MB (79.42%)

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int n= nums.length;
+        int c=0;
+        for(int i=0;i<n-2;i++){
+            if(nums[i]==0){
+                nums[i]^=1;
+                nums[i+1]^=1;
+                nums[i+2]^=1;
+                c++;
+            }
+        }
+
+        return(nums[n-2]==1 && nums[n-1]==1)?c:-1;
+    }
+}
